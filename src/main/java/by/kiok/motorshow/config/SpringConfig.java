270aea4a2f78472970426cbddc48e72f298ee337
Kihtenko Olga <kiok.ko1811@gmail.com>
2024-12-09 06:37:45 +0300

    refactor: delete comments

--- src/main/java/by/kiok/motorshow/config/SpringConfig.java
+++ src/main/java/by/kiok/motorshow/config/SpringConfig.java
@@ -9,10 +9,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
@@ -20,42 +17,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
+@EnableWebMvc
 @Configuration
+@RequiredArgsConstructor
 @EnableTransactionManagement
 @ComponentScan("by.kiok.motorshow")
 @PropertySource("classpath:application.yaml")
 @EnableJpaRepositories("by.kiok.motorshow.repository")
-@RequiredArgsConstructor
-@EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
 
     private final Environment environment;
-
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName(environment.getRequiredProperty("driver-class-name"));
-//        dataSource.setUrl(environment.getRequiredProperty("url"));
-//        dataSource.setUsername(environment.getRequiredProperty("username"));
-//        dataSource.setPassword(environment.getRequiredProperty("password"));
-//        return dataSource;
-//    }
-
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-//        emf.setDataSource(dataSource());
-//        emf.setPackagesToScan("by.kiok.motorshow");
-//        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-//        Properties props = new Properties();
-//        props.setProperty("hibernate.hbm2ddl.auto", "update");
-//        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-//        emf.setJpaProperties(props);
-//        return emf;
-//    }
 
     @Bean
     public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
